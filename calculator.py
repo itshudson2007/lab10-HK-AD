ab2b4d90bf9198f32210ad0ffdb8732daa8995c7
alans <alansoldaniel@gmail.com>
2025-11-13 15:00:48 -0500

    .

---
+++ calculator.py
@@ -0,0 +1,12 @@
+import math
+def add(a, b): a + b
+
+def sub(a, b): a - b
+
+def mul(a, b): a * b
+
+def div(a, b):
+
+def log(a, b): loga(b)# use math library + raise ValueError
+
+def exp(a, b): ab
