2759d1fe2d5113b7a47f246a647d9f0a120ae455
Hudson Kennedy <hudsonkennedy@ufl.edu>
2025-11-13 15:06:42 -0500

    modified calculator p1

---
+++ calculator.py
@@ -0,0 +1,21 @@
+import math
+
+def logarithm(x, y):
+    if x <= 0 or x == 1 or y <= 0:
+        raise ValueError("Invalid input for logarithm: Base must be positive and not 1, and the number must be positive.")
+    return math.log(x, y)
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def exponent(x, y):
+    return x ** y
