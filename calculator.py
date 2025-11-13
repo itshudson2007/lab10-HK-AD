74435dadaf3375978826a194aed41b253bb7ef89
alans <alansoldaniel@gmail.com>
2025-11-13 15:11:24 -0500

    .

--- calculator.py
+++ calculator.py
@@ -1,12 +1,21 @@
 import math
-def add(a, b): a + b
+def add(a, b):
+    return a + b
 
-def sub(a, b): a - b
+def sub(a, b):
+    return a - b
 
-def mul(a, b): a * b
+def mul(a, b):
+    return a * b
 
 def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by zero")
+    return a / b
 
-def log(a, b): loga(b)# use math library + raise ValueError
+def logarithm(a, b):
+    return math.log(b, a)
 
-def exp(a, b): ab
+def exponent(a, b):
+    return a**b
+
